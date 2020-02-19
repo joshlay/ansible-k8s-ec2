5671b2d892b584d116e55a17cf9a97416ebebac8
Pete Travis <pete.travis@rackspace.com>
2020-02-18 23:17:48 -0600

    resets

--- reset_cluster.sh
+++ reset_cluster.sh
@@ -5,5 +5,4 @@ sed -i -e 's/^s/#s/' ansible.cfg
 ansible-playbook all.yml --limit ec2-etcd --tags always
 sed -i -e 's/^#s/s/' ansible.cfg
 ansible-playbook all.yml --limit ec2-etcd --tags common,etcd
-ansible-playbook all.yml --tags kube_builder,elb,ingress,worker
-ansible-playbook all.yml --tags canary
+ansible-playbook all.yml --skip-tags common,etcd
