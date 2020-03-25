94fa263e9e8097e77ff7888dd0c639e109ec4e06
Pete Travis <pete.travis@rackspace.com>
2020-03-24 20:57:21 -0500

    use autoscaling groups

--- roles/etcd_autoscale/image_prep/files/etcd_bootstrap.py
+++ roles/etcd_autoscale/image_prep/files/etcd_bootstrap.py
@@ -26,6 +26,31 @@ class etcd_cert_helper(object):
         
         self.session = Session(region_name=self.i['region'])
         self.acm = self.session.client('acm')
+        self.autoscaling = self.session.client('autoscaling')
+        self.ec2 = self.session.client('ec2')
+    def get_autoscaling_peer_ips(self):
+        parent_asg = self.autoscaling.describe_auto_scaling_instances(InstanceIds=[self.i["instanceId"]])
+		my_asg_name = parent_asg['AutoScalingInstances'][0]['AutoScalingGroupName']
+        my_asg = self.autoscaling.describe_auto_scaling_groups(AutoScalingGroupNames=[my_asg_name])
+	    peer_ids = [ instance['InstanceId'] for instance in my_asg['AutoScalingGroups'][0]['Instances'] ]	
+        peers = self.ec2.describe_instances(InstanceIds=peer_ids)
+        peer_ips = [ peer['Instances'][0]['PrivateIpAddress'] for peer in peers['Reservations'] ]
+		return peer_ips
+	def get_initial_cluster_string(self):
+        peer_ips = self.get_autoscaling_peer_ips()
+        initial_cluster = 'https://'.join([ ip + ":2380," for ip in peer_ips ]).strip(',')
+        return initial_cluster
+    def render_manifest(self):
+		f = open('/usr/local/share/etcd_autoscale/manifest_template.j2')
+        t = f.read()
+        f.close()
+        template = Template(t)
+        manifest = template.render(
+          hostname = self.i['hostname']
+          private_ip = self.i['private_ip']
+          initial_cluster = self.get_initial_cluster_string()
+		  )
+		return manifest
     def create_certs(self):
         print("creating certs")
         pass
