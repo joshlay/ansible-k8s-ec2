8a462b47961828ba611b068a2c6d5e5937bf5141
Pete Travis <pete.travis@rackspace.com>
2020-07-28 14:53:15 -0500

    get tags from boto instead of cli

--- roles/cluster_common/files/cluster_bootstrap.py
+++ roles/cluster_common/files/cluster_bootstrap.py
@@ -15,41 +15,40 @@ class etcd_helper(object):
         if r.ok:
             self.i = json.loads(r.content)
             self.i['hostname'] = os.getenv('HOSTNAME')
-            self.tags = {}
-            tag_command = f'aws --region { self.i["region"] } ec2 describe-tags --filters Name=resource-id,Values={self.i["instanceId"]}'
-            for t in json.loads(subprocess.check_output(tag_command.split()).decode('utf8'))['Tags']:
-                self.tags[t['Key']] = t['Value']
-            self.cluster = self.tags['KubernetesCluster']
         else:
             print("unable to retrieve instance metadata, exiting")
             sys.exit(1)
-        
         self.session = Session(region_name=self.i['region'])
         self.autoscaling = self.session.client('autoscaling')
         self.ec2 = self.session.client('ec2')
+        self.tags = {}
+        rawtags = self.ec2.describe_tags(DryRun=False, Filters=[ { 'Name': 'resource-id', 'Values': [self.i["instanceId"]]} ] )
+        for tag in rawtags['Tags']:
+            self.tags[tag['Key']] = tag['Value']
+        self.cluster = self.tags['KubernetesCluster']
     def get_autoscaling_peer_ips(self):
         parent_asg = self.autoscaling.describe_auto_scaling_instances(InstanceIds=[self.i["instanceId"]])
-		my_asg_name = parent_asg['AutoScalingInstances'][0]['AutoScalingGroupName']
+        my_asg_name = parent_asg['AutoScalingInstances'][0]['AutoScalingGroupName']
         my_asg = self.autoscaling.describe_auto_scaling_groups(AutoScalingGroupNames=[my_asg_name])
-	    peer_ids = [ instance['InstanceId'] for instance in my_asg['AutoScalingGroups'][0]['Instances'] ]	
+        peer_ids = [ instance['InstanceId'] for instance in my_asg['AutoScalingGroups'][0]['Instances'] ]    
         peers = self.ec2.describe_instances(InstanceIds=peer_ids)
         peer_ips = [ peer['Instances'][0]['PrivateIpAddress'] for peer in peers['Reservations'] ]
-		return peer_ips
-	def get_initial_cluster_string(self):
+        return peer_ips
+    def get_initial_cluster_string(self):
         peer_ips = self.get_autoscaling_peer_ips()
         initial_cluster = 'https://'.join([ ip + ":2380," for ip in peer_ips ]).strip(',')
         return initial_cluster
     def render_manifest(self):
-		f = open('/usr/local/share/etcd_autoscale/manifest_template.j2')
+        f = open('/usr/local/share/etcd_autoscale/manifest_template.j2')
         t = f.read()
         f.close()
         template = Template(t)
         manifest = template.render(
-          hostname = self.i['hostname']
-          private_ip = self.i['private_ip']
+          hostname = self.i['hostname'],
+          private_ip = self.i['private_ip'],
           initial_cluster = self.get_initial_cluster_string()
-		  )
-		return manifest
+          )
+        return manifest
     def create_certs(self):
         print("creating certs")
         create_invocation = subprocess.check_call('kubeadm init phase certs etcd-ca')
